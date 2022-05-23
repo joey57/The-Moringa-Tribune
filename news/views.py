b27c727c3834c622697718dce71e182d5bd9d401
joey57 <njorogejoey5@gmail.com>
2022-05-23 13:57:42 +0300

    template inheritance

--- news/views.py
+++ news/views.py
@@ -1,32 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-  return HttpResponse("Welcome to Moringa Tribune")
+  return render( request, 'welcome.html')
 
 def news_of_day(request):
   date = dt.date.today()
-  # Function to convert date object to find exact day
-  day = convert_dates(date)
-  html = f'''
-      <html>
-        <body>
-           <h1>News for{day} {date.day}-{date.month}-{date.year}</h1>
-        </body>
-      </html>  
-          '''
-  return HttpResponse(html) 
-
-def convert_dates(dates):
-  #function that gets the weekday number for the date.
-  day_number = dt.date.weekday(dates)
-
-  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  # Returning the actual day of the week
-  day = days[day_number]
-  return day
+  return render(request, 'all-news/today-news.html', {"date": date,}) 
 
 def past_days_news(request, past_date):
   try:
@@ -35,15 +17,9 @@ def past_days_news(request, past_date):
   except ValueError:
       #raise 404 error when ValueError is thrown
       raise Http404()
-
-  day = convert_dates(date)
-  html = f'''
-       <html>
-        <body>
-          <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-        </body>
-       </html>
-          '''
-  return HttpResponse(html)
+      assert False
+  if date == dt.date.today():
+    return redirect(news_of_day)    
+  return render(request, 'all-news/past-news.html', {"date": date})
 
 
