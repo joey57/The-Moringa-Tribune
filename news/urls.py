f8dab4a10f8b24e34010481e130668e835e289bc
joey57 <njorogejoey5@gmail.com>
2022-05-23 18:10:50 +0300

    add re_path

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
   path('welcome/', views.welcome, name = 'welcome'),
   path('today/',views.news_of_day , name='newsToday'),
-  path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews')
+  re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews')
 ]
  
