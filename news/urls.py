5865e57041940f8b67458222178d8bcb2773e482
joey57 <njorogejoey5@gmail.com>
2022-05-23 12:27:52 +0300

    adding dynamic urls

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
   path('welcome/', views.welcome, name = 'welcome'),
-  path('today/',views.news_of_day , name='newsToday')
+  path('today/',views.news_of_day , name='newsToday'),
+  path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews')
 ]
  
