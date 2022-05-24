d3fbe255d9832625ff92a42486be58b41cf67b93
joey57 <njorogejoey5@gmail.com>
2022-05-24 13:22:24 +0300

    display model data

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Editor, Article,tags
+import datetime as dt
 
 # Create your tests here.
 class EditorTestClass(TestCase):
@@ -35,5 +36,15 @@ class ArticleTestClass(TestCase):
     Editor.objects.all().delete()
     tags.objects.all().delete()
     Article.objects.all().delete()
-      
+
+  def test_get_news_today(self):
+    today_news = Article.todays_news()
+    self.assertTrue(len(today_news) > 0) 
+
+  def test_get_news_by_date(self):
+    test_date = '2017-03-17'
+    date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+    news_by_date = Article.days_news(date)
+    self.assertTrue(len(news_by_date) == 0)   
+
 
