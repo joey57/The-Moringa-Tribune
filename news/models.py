524cb229b832e8dc02fe31e7d05d51c76d8b18ea
joey57 <njorogejoey5@gmail.com>
2022-05-24 12:20:11 +0300

    testing models

--- news/models.py
+++ news/models.py
@@ -9,8 +9,10 @@ class Editor(models.Model):
   def __str__(self):
     return self.first_name
   class Meta:
-    ordering = ['first_name']
-
+    ordering = ['first_name']  
+  def save_editor(self):
+    self.save()  
+     
 class tags(models.Model):
   name = models.CharField(max_length=30)
 
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Editor, Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+  #set up method
+  def setUp(self):
+    self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+
+  # testing instance
+  def test_instance(self):
+    self.assertTrue(isinstance(self.james, Editor))  
+
+  def test_save_method(self):
+    self.james.save_editor()
+    editors = Editor.objects.all()
+    self.assertTrue(len(editors) > 0)  
+
