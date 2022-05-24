73902edc3749178ba5f03d82c6a41612d3ec87fa
joey57 <njorogejoey5@gmail.com>
2022-05-24 13:07:20 +0300

    create articles test

--- news/tests.py
+++ news/tests.py
@@ -4,7 +4,7 @@ from .models import Editor, Article,tags
 # Create your tests here.
 class EditorTestClass(TestCase):
   #set up method
-  def setUp(self):
+  def setUp(self):  
     self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
 
   # testing instance
@@ -15,4 +15,25 @@ class EditorTestClass(TestCase):
     self.james.save_editor()
     editors = Editor.objects.all()
     self.assertTrue(len(editors) > 0)  
+
+class ArticleTestClass(TestCase):
+  def setUp(self):
+    #creating a new editor and saving it
+    self.james = Editor(first_name = 'James', last_name = 'Muriuki', email = 'james@moringaschool.com')
+    self.james.save_editor()
+
+    #creating a new tag and saving it 
+    self.new_tag = tags (name = 'testing')
+    self.new_tag.save()
+
+    self.new_article = Article(title = 'Test Article', post = 'This is a random test Post', editor = self.james)
+    self.new_article.save()
+
+    self.new_article.tags.add(self.new_tag)
+
+  def tearDown(self):
+    Editor.objects.all().delete()
+    tags.objects.all().delete()
+    Article.objects.all().delete()
+      
 
