3c6b80f1841ad947b95edeec11c3c3f07cd0907f
joey57 <njorogejoey5@gmail.com>
2022-05-24 12:41:41 +0300

    change site url to main page

--- news/urls.py
+++ news/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
-  path('welcome/', views.welcome, name = 'welcome'),
-  path('today/',views.news_of_day , name='newsToday'),
+  path('',views.news_of_day , name='newsToday'),
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews')
 ]
  
