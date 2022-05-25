2f3293662d5bf6239bd235bc400f598090662843
joey57 <njorogejoey5@gmail.com>
2022-05-25 08:06:32 +0300

    display news on template

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
-  path('',views.news_of_day , name='newsToday'),
+  path('',views.news_today , name='newsToday'),
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews')
 ]
  
--- news/views.py
+++ news/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
-def news_of_day(request):
+def news_today(request):
   date = dt.date.today()
-  return render(request, 'all-news/today-news.html', {"date": date,}) 
+  news = Article.todays_news()
+  return render(request, 'all-news/today-news.html', {"date": date, "news": news}) 
 
 def past_days_news(request, past_date):
   try:
@@ -16,7 +18,9 @@ def past_days_news(request, past_date):
       raise Http404()
       assert False
   if date == dt.date.today():
-    return redirect(news_of_day)    
-  return render(request, 'all-news/past-news.html', {"date": date})
+    return redirect(news_today)
+
+  news = Article.days_news(date)    
+  return render(request, 'all-news/past-news.html', {"date": date, "news":news})
 
 
