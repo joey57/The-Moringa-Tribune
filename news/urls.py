c0f52c67ec24e47bc406cb1521d2f4a5f0989da8
joey57 <njorogejoey5@gmail.com>
2022-05-25 09:59:19 +0300

    uploading images

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
   path('',views.news_today , name='newsToday'),
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews'),
   path('search/', views.search_results, name = 'search_results')
 ]
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
  
