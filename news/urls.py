d8bb69e4f16ec7ec2ac787f6ae85cf1ca9f53f7d
joey57 <njorogejoey5@gmail.com>
2022-05-25 08:43:51 +0300

    search for titles in form

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
   path('',views.news_today , name='newsToday'),
-  re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews')
+  re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews'),
+  path('search/', views.search_results, name = 'search_results')
 ]
  
--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from email import message
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
@@ -22,5 +23,16 @@ def past_days_news(request, past_date):
 
   news = Article.days_news(date)    
   return render(request, 'all-news/past-news.html', {"date": date, "news":news})
+
+def search_results(request):
+  if 'article' in request.GET and request.GET["article"]:
+    search_term = request.GET.get("article")
+    searched_articles = Article.search_by_title(search_term)
+    message = f"{search_term}"
+
+    return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+  else:
+     message = "You haven't searched for any term"
+     return render(request, 'all-news/search.html',{"message":message})  
 
 
