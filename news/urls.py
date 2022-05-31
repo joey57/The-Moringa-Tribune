5daafec4f39d2e71f2989354510fe2bcd04351f7
joey57 <njorogejoey5@gmail.com>
2022-05-31 12:18:22 +0300

    django-registration

--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,7 +7,8 @@ urlpatterns=[
   path('',views.news_today , name='newsToday'),
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news, name = 'pastNews'),
   path('search/', views.search_results, name = 'search_results'),
-  re_path('article/(\d+)',views.article , name = 'article')
+  re_path('article/(\d+)',views.article , name = 'article'),
+  path('accounts/', include('registration.backends.simple.urls')),
 ]
 if settings.DEBUG:
   urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
