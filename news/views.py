86395c32c38a88b568ec1a473a7dadae0419b2ae
joey57 <njorogejoey5@gmail.com>
2022-05-31 13:01:18 +0300

    django authentication

--- news/views.py
+++ news/views.py
@@ -4,6 +4,7 @@ import datetime as dt
 from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def news_today(request):
@@ -49,18 +50,12 @@ def search_results(request):
     return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
   else:
      message = "You haven't searched for any term"
-     return render(request, 'all-news/search.html',{"message":message})  
+     return render(request, 'all-news/search.html',{"message":message}) 
 
+@login_required(login_url='/accounts/login/')
 def article(request,article_id):
   try:
     article = Article.objects.get(id = article_id)
   except:
     raise Http404()
-  return render(request,'all-news/article.html', {"article":article}) 
-
-
-
-
-
-
-
+  return render(request,'all-news/article.html', {"article":article})
