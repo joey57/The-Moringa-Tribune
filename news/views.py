e4fbc99f956ea1b5febe3f93896556a728eb53f7
joey57 <njorogejoey5@gmail.com>
2022-06-06 13:42:46 +0300

    update new today to display only form and newsletter to get email. name for ajax request

--- news/views.py
+++ news/views.py
@@ -1,30 +1,18 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect, JsonResponse
 import datetime as dt
 from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm, NewsArticleForm
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 def news_today(request):
   date = dt.date.today()
   news = Article.todays_news()
+  form = NewsLetterForm()
 
-  if request.method == 'POST':
-    form = NewsLetterForm(request.POST)
-    if form.is_valid():
-      name = form.cleaned_data['your_name']
-      email = form.cleaned_data['email']
-      
-      recipient = NewsLetterRecipients(name = name, email = email)
-      recipient.save()
-      send_welcome_email(name, email)
-
-      HttpResponseRedirect('news_today')
-  else:
-      form = NewsLetterForm()
-      
   return render(request, 'all-news/today-news.html', {"date": date, "news": news, "letterForm":form}) 
 
 def past_days_news(request, past_date):
@@ -72,5 +60,15 @@ def new_article(request):
     return redirect('newsToday')
   else:
     form = NewsArticleForm()
-  return render(request, 'new_article.html',{"form":form})      
+  return render(request, 'new_article.html',{"form":form})   
+
+def newsletter(request):
+  name = request.POST.get('your_name')
+  email = request.POST.get('email')
+
+  recipient = NewsLetterRecipients(name =  name, email = email)
+  recipient.save()
+  send_welcome_email(name, email)
+  data = {'Success': 'You have been successfully added to mailing list'}
+  return JsonResponse(data)
 
