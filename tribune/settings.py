bfa42761f85b84d15c08282a805ad1ea5690b96e
joey57 <njorogejoey5@gmail.com>
2022-06-06 13:43:01 +0300

    add logout url route

--- tribune/settings.py
+++ tribune/settings.py
@@ -163,5 +163,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'newsToday'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = '/'
+
 # Configure Django app for Heroku 
 django_heroku.settings(locals())
