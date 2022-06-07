9d7f6f6400154fbedd46f5c634c45c9358c36428
joey57 <njorogejoey5@gmail.com>
2022-06-07 11:55:20 +0300

    handling post request

--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from os import stat
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect, JsonResponse
 import datetime as dt
@@ -8,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import MerchSerializer
-
+from rest_framework import status
 
 
 # Create your views here.
@@ -81,3 +82,10 @@ class MerchList(APIView):
     all_merch = MoringaMerch.objects.all()
     serializers = MerchSerializer(all_merch, many=True)
     return Response(serializers.data)
+
+  def post(self, request, format=None):
+    serializers = MerchSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
