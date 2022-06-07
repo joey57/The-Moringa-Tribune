9a360c68bed8afe7b415da779161f1afc64ee055
joey57 <njorogejoey5@gmail.com>
2022-06-07 12:35:46 +0300

    api authentication

--- tribune/settings.py
+++ tribune/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'tinymce',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 
 # Internationalization
